0517ee873da4ba69e228a5d37718ac2aaf6be560
MazenAtlam <mazenatef5510@gmail.com>
2025-01-02 01:20:38 +0200

    Add docstrings to models.py for Booking, Menu, and Category models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,21 +1,93 @@
+"""
+This module defines the models for the restaurant application.
+"""
+
 from django.db import models
 
 
-# Create your models here.
 class Booking(models.Model):
+    """
+    Booking model represents a reservation made by a customer.
+
+    Attributes:
+        first_name (CharField): The first name of the customer making the reservation.
+            Maximum length is 200 characters.
+        reservation_date (DateField): The date of the reservation.
+        reservation_slot (SmallIntegerField): The time slot for the reservation.
+            Default value is 10.
+
+    Methods:
+        __str__(): Returns the first name of the customer.
+    """
+
     first_name = models.CharField(max_length=200)
     reservation_date = models.DateField()
     reservation_slot = models.SmallIntegerField(default=10)
 
-    def __str__(self): 
-        return self.first_name
+    def __str__(self):
+        """
+        A string representation of the object
+
+        Returns:
+            str: The first name of the object.
+        """
+
+        return str(self.first_name)
 
 
-# Add code to create Menu model
 class Menu(models.Model):
-   name = models.CharField(max_length=200) 
-   price = models.IntegerField(null=False) 
-   menu_item_description = models.TextField(max_length=1000, default='') 
+    """A Django model representing a menu item in a restaurant.
 
-   def __str__(self):
-      return self.name
+    Attributes:
+        name (CharField): The name of the menu item, with a maximum length of 200 characters.
+        price (IntegerField): The price of the menu item, which cannot be null.
+        menu_item_description (TextField): A description of the menu item,
+            with a maximum length of 1000 characters.
+        Category (ForeignKey): A foreign key to the Category model,
+            with a cascade delete option and a related name 'menu_items'.
+
+    Methods:
+        __str__(): Returns the name of the menu item as its string representation.
+    """
+
+    name = models.CharField(max_length=200)
+    price = models.IntegerField(null=False)
+    menu_item_description = models.TextField(max_length=1000, default='')
+    Category = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE,
+        related_name='menu_items', default=0)
+
+    def __str__(self):
+        """
+        A string representation of the object
+
+        Returns:
+            str: The first name of the object.
+        """
+
+        return str(self.name)
+
+
+class Category(models.Model):
+    """Category model to represent different categories in the restaurant.
+
+    Attributes:
+        name (str): The name of the category.
+            It must be unique and can have a maximum length of 50 characters.
+
+    Methods:
+        __str__(): Returns the name of the category as its string representation.
+    """
+
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        """
+        A string representation of the object
+
+        Returns:
+            str: The first name of the object.
+        """
+
+        return str(self.name)
