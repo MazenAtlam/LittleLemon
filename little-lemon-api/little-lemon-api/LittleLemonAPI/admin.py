496d9cf0da68c3c3e4c54573b724e9d418793585
Ahmed Shehab <ahmed.shehab06@eng-st.cu.edu.eg>
2025-01-19 06:21:08 +0000

    Refactored

--- little-lemon-api/little-lemon-api/LittleLemonAPI/admin.py
+++ little-lemon-api/little-lemon-api/LittleLemonAPI/admin.py
@@ -5,14 +5,17 @@ from . import models
 
 # Register your models here.
 
+
 class MenuItemAdmin(admin.ModelAdmin):
     list_display = ["title", "price", "featured", "category"]
     list_filter = ["category"]
     search_fields = ["title"]
 
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ["title", "slug"]
     search_fields = ["title"]
+
 
 admin.site.register(models.Cart)
 admin.site.register(models.Order)
